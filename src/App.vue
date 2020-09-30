4ea3b4dce55360dce31ee385698fa85cd8dc6896
Seksak <bjbandej@gmail.com>
2020-09-30 16:18:57 +0700

    ....

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+   <KoolKeeper aaa="fdsfs"/>
+  </div>
+</template>
+
+<script>
+import KoolKeeper from './components/KoolKeeper'
+
+
+export default {
+  name: 'App',
+  components: {
+    KoolKeeper
+  }
+}
+</script>
+
+<style>
+/* Import Bootstrap Vue Styles */
+/* @import "~bootstrap-vue/dist/bootstrap-vue.css"; */
+@import '~bootstrap/dist/css/bootstrap.css';
+@import '~bootstrap-vue/dist/bootstrap-vue.css';
+
+</style>
